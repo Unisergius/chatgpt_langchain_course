3dc16a3083aed1079edb7b3b9d94460d0eafa630
shenriquesdev <Unisergius@gmail.com>
2024-03-04 21:44:17 +0000

    add 2 lessons

---
+++ pycode/main.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+import argparse
+
+load_dotenv()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--task", default="return a list of numbers", type=str, help="The task you want to perform")
+parser.add_argument("--language", default="python", type=str, help="The language you want to use")
+args = parser.parse_args()
+
+#OPENAI procura por uma chave de API no arquivo .env automaticamente
+llm = OpenAI()
+
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"],
+)
+
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test in {language} that tests the following {language} code:\n{code}"
+)
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+)
+
+result = chain({
+    "language": args.language,
+    "task": args.task
+})
+
+print("---------CODE---------")
+
+print(result["code"])
+
+print("---------TEST---------")
+print(result["test"])
---
+++ tchat/main.py
@@ -0,0 +1,48 @@
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
+from dotenv import load_dotenv
+# We will use a chat based model, instead of the completionist model
+# System string - sets up what the LLM role will be
+# User message (in LangChain its Human) - message going from the user to the llm
+# Assistant message (in LangChain its AI) - reply message going from the llm to the user
+
+# Everytime we chat with the LLM, we will sending the whole history of the conversation to it
+
+load_dotenv()
+
+chat = ChatOpenAI(verbose=True)
+
+memory = ConversationSummaryMemory(
+    llm=chat,
+    memory_key="messages", 
+    return_messages=True, 
+    chat_memory=FileChatMessageHistory("messages.json")
+)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory,
+    verbose=True
+)
+
+#"You are a chatbot specializing in {subject}"
+
+#"Tell me why I "
+
+while True:
+    content = input(">> ")
+    if content == "exit":
+        break
+    result = chain({"content": content})
+    print(result["text"])
